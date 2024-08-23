469fbdb58abc3bad98cf4e6afc852da89892dd86
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2024-08-23 17:36:59 +0800

    kickstart config using kickstart-nix template

--- flake.nix
+++ flake.nix
@@ -2,38 +2,76 @@
   description = "neovim with preset config in a nix flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    gen-luarc.url = "github:mrcjkb/nix-gen-luarc-json";
+
+    # Add bleeding-edge plugins here.
+    # They can be updated with `nix flake update` (make sure to commit the generated flake.lock)
+    # wf-nvim = {
+    #   url = "github:Cassin01/wf.nvim";
+    #   flake = false;
+    # };
   };
 
-  outputs = {
+  outputs = inputs @ {
     self,
     nixpkgs,
+    flake-utils,
+    gen-luarc,
+    ...
   }: let
-    system = "x86_64-linux";
+    supportedSystems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
 
-    pkgs = import nixpkgs {
-      inherit system;
-    };
+    # This is where the Neovim derivation is built.
+    neovim-overlay = import ./nix/neovim-overlay.nix {inherit inputs;};
+  in
+    flake-utils.lib.eachSystem supportedSystems (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          # Import the overlay, so that the final Neovim derivation(s) can be accessed via pkgs.<nvim-pkg>
+          neovim-overlay
+          # This adds a function can be used to generate a .luarc.json
+          # containing the Neovim API all plugins in the workspace directory.
+          # The generated file can be symlinked in the devShell's shellHook.
+          gen-luarc.overlays.default
+        ];
+      };
+      shell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          stylua
+          selene
+          lua-language-server
+          luajitPackages.luacheck
 
-    inherit (nixpkgs) lib;
-  in {
-    packages.${system}.default = (import ./neovim {inherit pkgs;}).nvim-arst;
-    apps.${system}.default = {
-      type = "app";
-      program = lib.getExe (import ./neovim {inherit pkgs;}).nvim-arst;
+          alejandra
+          statix
+          deadnix
+          nil
+        ];
+        formatter = pkgs.alejandra;
+        shellHook = ''
+          # symlink the .luarc.json generated in the overlay
+          ln -fs ${pkgs.nvim-luarc-json} .luarc.json
+        '';
+      };
+    in {
+      packages = rec {
+        default = nvim;
+        nvim = pkgs.nvim-pkg;
+      };
+      devShells = {
+        default = shell;
+      };
+    })
+    // {
+      # You can add this overlay to your NixOS configuration
+      overlays.default = neovim-overlay;
     };
-    devShells.${system}.default = pkgs.mkShell {
-      packages = with pkgs; [
-        stylua
-        selene
-        lua-language-server
-
-        alejandra
-        statix
-        deadnix
-        nil
-      ];
-      formatter = pkgs.alejandra;
-    };
-  };
 }
