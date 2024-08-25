410d93d2583519f7fa03ed2647a80ed58944a1d4
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2024-08-25 14:16:12 +0800

    remove eyeliner-nvim

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -63,7 +63,6 @@ with final.pkgs.lib; let
     # ^ language support
     # navigation/editing enhancement plugins
     vim-unimpaired # predefined ] and [ navigation keymaps | https://github.com/tpope/vim-unimpaired/
-    eyeliner-nvim # Highlights unique characters for f/F and t/T motions | https://github.com/jinh0/eyeliner.nvim
     nvim-surround # https://github.com/kylechui/nvim-surround/
     nvim-treesitter-textobjects # https://github.com/nvim-treesitter/nvim-treesitter-textobjects/
     nvim-ts-context-commentstring # https://github.com/joosepalviste/nvim-ts-context-commentstring/
